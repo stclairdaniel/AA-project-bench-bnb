d26a2b40db3c4de69d2eb31ba9b902cb35e5d6cd
Daniel St. Clair <stclair.daniel@gmail.com>
2016-08-26 12:10:38 -0700

    setup backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Bench.create(description: 'my house', lat: 37.789727, lng: -122.427042)
+Bench.create(description: 'App Academy', lat: 37.791253, lng: -122.393630)
+Bench.create(description: 'Golden Gate Bridge', lat: 37.810986, lng: -122.477625)
