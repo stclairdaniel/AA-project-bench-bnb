2af3bddd8ce45c4d6a8cbb409ff804abd1277fa9
Daniel St. Clair <stclair.daniel@gmail.com>
2016-08-26 13:11:02 -0700

    webpack and package.json setup. first render

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  ReactDom.render(<div>It worked</div>, root);
+});
